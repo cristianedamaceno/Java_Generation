64a6aa2c1bfdda24b54250d9373427dceaeef789
Cristiane Damaceno <cristinecarmozini@gmail.com>
2021-11-10 16:25:58 -0300

    Laços repetição Java

---
+++ Exercicios Laço de Repetição/exRepetição01.java
@@ -0,0 +1,34 @@
+package exercicioIdade;
+
+import java.util.Scanner;
+
+public class exRepetição01 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		System.out.println( "\n Informe números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5");
+		
+		 
+		 
+		 
+		 
+		 
+		for (int i=1000; i < 2000; i++){
+		    if (i%11==5) {
+		        System.out.println(i);
+					}
+				}
+		
+		
+	}
+	
+	
+}
+
+		
+	
